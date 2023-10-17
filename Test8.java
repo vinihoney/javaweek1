53e9970b7377d960585b7d165a0c485404a95cfb
vinihoney <sonessel@gmail.com>
2023-10-18 00:14:08 +0100

    pushing homework

---
+++ Test8.java
@@ -0,0 +1,15 @@
+public class Test8 {
+    public static void main(String[] args){
+        //int n1 = 74, n2 =36, divide;
+        //divide = n1 / n2;
+        //System.out.println("Test Data:"+ n1);
+        //System.out.println("Test Data:"+ n2);
+        //System.out.println("Test Data:"+ n1,n2);
+        //System.out.println("The sum of numbers is: "+sum); // sum of n1+n2
+            int n1 = 50;
+            int n2 = 3;
+            int area = n1 / n2;
+            System.out.println("Area of rectangle: " + area);
+        }
+
+}
