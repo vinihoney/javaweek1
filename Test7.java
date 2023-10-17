faa81058d0fd8f7acc938eee2521d68ba70455da
vinihoney <sonessel@gmail.com>
2023-10-17 23:58:52 +0100

    pushing homework

---
+++ Test7.java
@@ -0,0 +1,10 @@
+public class Test7 {
+    public static void main(String[] args){
+        int n1 = 74, n2 =36, sum;
+        sum = n1 + n2;
+        System.out.println("Test Data:"+ n1);
+        System.out.println("Test Data:"+ n2);
+        //System.out.println("Test Data:"+ n1,n2);
+        System.out.println("The sum of numbers is: "+sum); // sum of n1+n2
+    }
+}
